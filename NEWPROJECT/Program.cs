b0d5a77a99b211ceea38ab8b79ae16decd0d40a8
Steve Sutton <steven.p.sutton@gmail.com>
2020-10-06 10:54:47 +0100

    Initial creation

---
+++ NEWPROJECT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NEWPROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
